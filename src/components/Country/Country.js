6a6785fb896f63244c25b13ded7ce6d3a889473a
Md-Azad <sokalazad@gmail.com>
2023-03-22 23:54:41 +0100

    added some css

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Country.css";
 
 const Country = (props) => {
   return (
-    <div>
+    <div className="country">
       <h3>Country Name: {props.name}</h3>
       <p>Population: {props.population}</p>
       <p>Area: {props.area}</p>
