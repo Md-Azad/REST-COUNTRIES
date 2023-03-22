8fb8eadea00b6f1d98618a5d013663fdff94bf46
Md-Azad <sokalazad@gmail.com>
2023-03-23 00:13:27 +0100

    added object destructuring

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,12 +3,22 @@ import "./Country.css";
 
 const Country = (props) => {
   console.log(props.country);
+  //   Objecty destructuring
+  const { name, region, population, area } = props.country;
   return (
     <div className="country">
-      <h3>Country Name: {props.country.name.common}</h3>
+      {/* Without destructuring */}
+      {/* <h3>Country Name: {props.country.name.common}</h3>
       <p>Region: {props.country.region}</p>
       <p>Population: {props.country.population}</p>
-      <p>Area: {props.country.area}</p>
+      <p>Area: {props.country.area}</p> */}
+
+      {/* with destructuring */}
+
+      <h3>Country Name: {name.common}</h3>
+      <p>Region: {region}</p>
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
     </div>
   );
 };
